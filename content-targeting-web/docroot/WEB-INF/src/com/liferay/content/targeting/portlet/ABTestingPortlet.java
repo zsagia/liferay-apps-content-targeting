0ef2b96484754484eb897d36b15d7eee8e99f58f
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-12-19 10:51:21 -0800

    Add AB Testing portlet

---
+++ content-targeting-web/docroot/WEB-INF/src/com/liferay/content/targeting/portlet/ABTestingPortlet.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.targeting.portlet;
+
+import com.liferay.content.targeting.portlet.util.QueryRule;
+import com.liferay.content.targeting.portlet.util.UserSegmentQueryRule;
+import com.liferay.content.targeting.portlet.util.UserSegmentQueryRuleUtil;
+import com.liferay.content.targeting.util.ContentTargetingUtil;
+import com.liferay.content.targeting.util.UserSegmentUtil;
+import com.liferay.content.targeting.util.WebKeys;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.template.Template;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.asset.AssetRendererFactoryRegistryUtil;
+import com.liferay.portlet.asset.model.AssetEntry;
+import com.liferay.portlet.asset.model.AssetRendererFactory;
+import freemarker.ext.beans.BeansWrapper;
+import freemarker.template.TemplateHashModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletPreferences;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class ABTestingPortlet extends CTFreeMarkerPortlet {
+
+	@Override
+	protected void doPopulateContext(
+			String path, PortletRequest portletRequest,
+			PortletResponse portletResponse, Template template)
+		throws Exception {
+
+		BeansWrapper wrapper = BeansWrapper.getDefaultInstance();
+
+		TemplateHashModel staticModels = wrapper.getStaticModels();
+
+		template.put("currentURL", PortalUtil.getCurrentURL(portletRequest));
+		template.put(
+			"redirect", ParamUtil.getString(portletRequest, "redirect"));
+
+		populateViewContext(
+			path, portletRequest, portletResponse, template, staticModels);
+	}
+
+	protected void populateViewContext(
+			String path, PortletRequest portletRequest,
+			PortletResponse portletResponse, Template template,
+			TemplateHashModel staticModels)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		PortletPreferences portletPreferences = portletRequest.getPreferences();
+
+		if (Validator.isNull(path) || path.equals(ABTestingPath.VIEW)) {
+
+		}
+		else if (path.equals(ABTestingPath.CONFIGURATION)) {
+
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(
+		ABTestingPortlet.class);
+
+}
