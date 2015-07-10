0471121eddd0684ea8aa8e7c1d49b6def6ed6469
Pavel Savinov <pavel.savinov@liferay.com>
2015-07-10 13:23:08 +0200

    WCM-403 additional tracking actions report test

--- report-campaign-tracking-action-test/test/integration/com/liferay/content/targeting/report/campaign/tracking/action/CTActionReportTest.java
+++ report-campaign-tracking-action-test/test/integration/com/liferay/content/targeting/report/campaign/tracking/action/CTActionReportTest.java
@@ -17,13 +17,16 @@ package com.liferay.content.targeting.report.campaign.tracking.action;
 import com.liferay.content.targeting.analytics.service.AnalyticsEventLocalService;
 import com.liferay.content.targeting.api.model.Report;
 import com.liferay.content.targeting.api.model.ReportsRegistry;
+import com.liferay.content.targeting.model.TrackingActionInstance;
 import com.liferay.content.targeting.model.UserSegment;
+import com.liferay.content.targeting.report.campaign.tracking.action.model.CTAction;
 import com.liferay.content.targeting.report.campaign.tracking.action.service.CTActionLocalService;
 import com.liferay.content.targeting.service.TrackingActionInstanceLocalService;
 import com.liferay.content.targeting.service.test.service.ServiceTestUtil;
 import com.liferay.content.targeting.service.test.util.TestPropsValues;
 import com.liferay.osgi.util.service.ServiceTrackerUtil;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.service.ServiceContext;
 
@@ -37,6 +40,9 @@ import org.junit.runner.RunWith;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleException;
+
+import java.util.Calendar;
+import java.util.List;
 
 /**
  * @author Eduardo Garcia
@@ -81,13 +87,17 @@ public class CTActionReportTest {
 
 		// Add tracking actions
 
-		_trackingActionInstanceLocalService.addTrackingActionInstance(
-			userId, "FormTrackingAction", campaignId, "Form alias", null, -1,
-			elementId, eventType, null, serviceContext);
+		TrackingActionInstance trackingActionInstanceForm =
+            _trackingActionInstanceLocalService.addTrackingActionInstance(
+                userId, "FormTrackingAction", campaignId,
+                StringUtil.randomString(), null, -1, elementId, eventType, null,
+                serviceContext);
 
-		_trackingActionInstanceLocalService.addTrackingActionInstance(
-			userId, "PageTrackingAction", campaignId, "Page alias", className,
-			classPK, null, eventType, StringPool.BLANK, serviceContext);
+		TrackingActionInstance trackingActionInstancePage =
+            _trackingActionInstanceLocalService.addTrackingActionInstance(
+                userId, "PageTrackingAction", campaignId,
+                StringUtil.randomString(), className, classPK, null, eventType,
+                StringPool.BLANK, serviceContext);
 
 		// Obtain report from registry
 
@@ -120,6 +130,122 @@ public class CTActionReportTest {
 		Assert.assertEquals(
 			initialCTActionCount + 2,
 			_ctActionLocalService.getCTActionsCount(campaignId));
+
+        _trackingActionInstanceLocalService.deleteTrackingActionInstance(
+            trackingActionInstanceForm);
+        _trackingActionInstanceLocalService.deleteTrackingActionInstance(
+            trackingActionInstancePage);
+
+        List<CTAction> ctActions =
+            _ctActionLocalService.getCTActions(campaignId);
+
+        for (CTAction ctAction : ctActions) {
+            _ctActionLocalService.deleteCTAction(ctAction);
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, 1);
+
+        _analyticsEventLocalService.deleteAnalyticsEvents(
+            serviceContext.getCompanyId(), calendar.getTime());
+	}
+
+	@Test
+	public void testRenameTrackingActionCTAReport() throws Exception {
+
+		ServiceContext serviceContext = ServiceTestUtil.getServiceContext();
+
+		long userId = TestPropsValues.getUserId();
+
+		long campaignId = 2;
+		String className = Layout.class.getName();
+		long classPK = 3;
+		String elementId = "form_id2";
+		String eventType = "view";
+
+		int initialCTActionCount = _ctActionLocalService.getCTActionsCount(
+			campaignId);
+
+		TrackingActionInstance trackingActionInstanceForm =
+			_trackingActionInstanceLocalService.addTrackingActionInstance(
+				userId, "FormTrackingAction", campaignId,
+                StringUtil.randomString(), null, -1, elementId, eventType, null,
+                serviceContext);
+
+		TrackingActionInstance trackingActionInstancePage =
+			_trackingActionInstanceLocalService.addTrackingActionInstance(
+				userId, "PageTrackingAction", campaignId,
+                StringUtil.randomString(), className, classPK, null, eventType,
+                StringPool.BLANK, serviceContext);
+
+		Report report = _reportsRegistry.getReport("CTActionReport");
+
+		report.updateReport(campaignId);
+
+		Assert.assertEquals(
+			initialCTActionCount,
+			_ctActionLocalService.getCTActionsCount(campaignId));
+
+		_analyticsEventLocalService.addAnalyticsEvent(
+			userId, 1, null, -1, UserSegment.class.getName(), new long[]{1},
+			elementId, eventType, "127.0.0.1", "ES", "User Agent",
+			"http://www.liferay.com", null, serviceContext);
+
+		_analyticsEventLocalService.addAnalyticsEvent(
+			userId, 1, className, classPK, UserSegment.class.getName(),
+			new long[]{2}, null, eventType, "127.0.0.1", "ES", "User Agent",
+			"http://www.liferay.com", null, serviceContext);
+
+		report.updateReport(campaignId);
+
+		Assert.assertEquals(
+			initialCTActionCount + 2,
+			_ctActionLocalService.getCTActionsCount(campaignId));
+
+		trackingActionInstanceForm.setAlias(StringUtil.randomString());
+		trackingActionInstancePage.setAlias(StringUtil.randomString());
+
+		_trackingActionInstanceLocalService.updateTrackingActionInstance(
+			trackingActionInstanceForm);
+		_trackingActionInstanceLocalService.updateTrackingActionInstance(
+			trackingActionInstancePage);
+
+		_analyticsEventLocalService.addAnalyticsEvent(
+			userId, 1, null, -1, UserSegment.class.getName(), new long[]{1},
+			elementId, eventType, "127.0.0.1", "ES", "User Agent",
+			"http://www.liferay.com", null, serviceContext);
+
+		_analyticsEventLocalService.addAnalyticsEvent(
+			userId, 1, className, classPK, UserSegment.class.getName(),
+			new long[]{2}, null, eventType, "127.0.0.1", "ES", "User Agent",
+			"http://www.liferay.com", null, serviceContext);
+
+		report.updateReport(campaignId);
+
+        // TA alias changed, but report still has the old one
+
+		Assert.assertEquals(
+			initialCTActionCount + 2,
+			_ctActionLocalService.getCTActionsCount(campaignId));
+
+        _trackingActionInstanceLocalService.deleteTrackingActionInstance(
+            trackingActionInstanceForm.getTrackingActionInstanceId());
+        _trackingActionInstanceLocalService.deleteTrackingActionInstance(
+            trackingActionInstancePage);
+
+        List<CTAction> ctActions =
+            _ctActionLocalService.getCTActions(campaignId);
+
+        for (CTAction ctAction : ctActions) {
+            _ctActionLocalService.deleteCTAction(ctAction);
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, 1);
+
+        _analyticsEventLocalService.deleteAnalyticsEvents(
+            serviceContext.getCompanyId(), calendar.getTime());
+
 	}
 
 	private AnalyticsEventLocalService _analyticsEventLocalService;
